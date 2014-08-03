5d19af1f0872aef20340458e100862bf018ffc01
Nick Price <neppers@gmail.com>
2014-08-03 19:22:12 +0100

    More stuff

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 var express = require('express'),
     app = express(),
+    fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    privateKey = fs.readFileSync('cert/kombi-key.pem'),
+    certificate = fs.readFileSync('cert/kombi-cert.pem'),
+    credentials = {key: privateKey, cert: certificate},
+    locals = require('./locals.json'),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
-    port = parseInt(process.env.PORT, 10) || 3000;
+    logger = require('morgan'),
+    compress = require('compression'),
+    favicon = require('serve-favicon'),
+    port = parseInt(process.env.PORT, 10) || 3000,
+    portSecure = parseInt(process.env.PORTS, 10) || 3030;
 
+// Settings
+app.set('views', __dirname + '/public');
+app.set('view engine', 'jade');
+
+// Middleware
+app.use(logger('dev'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(compress());
 app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static(__dirname + '/public'));
+
+// Routes
+app.get('/', function(req, res) {
+    res.render("index", locals);
+});
+app.use('/resources', express.static(__dirname + '/resources'));
+app.use('/src', express.static(__dirname + '/src'));
+app.get(/(.*)$/, function(req, res) {
+    res.render(req.params[0].substr(1), locals);
+});
+
+// Errors
 app.use(errorHandler({
     dumpExceptions: true,
     showStack: true
 }));
 
-console.log('Static server listening at localhost:' + port);
-app.listen(port);
+// Output
+var httpServer = http.createServer(app),
+    httpsServer = https.createServer(credentials, app);
+httpServer.listen(port);
+httpsServer.listen(portSecure);
+
+console.log('Server listening at http://localhost:' + port);
+console.log('Secure server listening at http://localhost:' + portSecure);
